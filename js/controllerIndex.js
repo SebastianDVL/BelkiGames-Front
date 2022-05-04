d4bccb40c9eb3189eb30a3e3511da19843255b93
johanfg33 <johanidp@hotmail.com>
2022-05-04 11:03:43 -0500

    Select de paises

--- js/controllerIndex.js
+++ js/controllerIndex.js
@@ -1,4 +1,10 @@
 import {changeForm} from './pagination.js'
-
+import {llenarPaises} from './paises.js'
 
 changeForm()
+let paises = await llenarPaises()
+
+paises.paises.forEach(pais =>{
+    let option = document.createElement("option")
+
+})
