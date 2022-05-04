aae4064dcd2ab570d80481c4c5f40833f2c057fc
Sebas <sebas.cay@gmail.com>
2022-05-04 14:23:37 -0500

    se configura select de paises con json y se obtiene la locacion del usuario con api de ip

--- js/controllerIndex.js
+++ js/controllerIndex.js
@@ -1,10 +1,7 @@
 import {changeForm} from './pagination.js'
-import {llenarPaises} from './paises.js'
+
 
 changeForm()
-let paises = await llenarPaises()
 
-paises.paises.forEach(pais =>{
-    let option = document.createElement("option")
 
-})
+
--- js/pagination.js
+++ js/pagination.js
@@ -1,4 +1,7 @@
-export function changeForm(){  
+import {llenarPaises} from './paises.js'
+import {getLocation} from './location.js'
+
+export async function changeForm(){  
     let log = document.querySelector("[data-log]")
     let sign = document.querySelector("[data-sign]")
 
@@ -8,6 +11,20 @@ export function changeForm(){
         let signForm = sign.content.cloneNode(true).children[0]
         form.appendChild(signForm)
         
+        let paises = await llenarPaises()
+        let location = await getLocation()
+        
+        paises.paises.forEach(pais =>{
+            let option = document.createElement("option")
+            if(location.country == pais.name){
+                option.selected = true
+            }
+            option.value = pais.code
+            option.textContent = pais.name
+
+            document.querySelector("#paises").appendChild(option)
+        })
+
         signForm.querySelector("#log").addEventListener("click", ()=>{
             window.location.href =(`./forms.html`)
         })
