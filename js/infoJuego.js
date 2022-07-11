09b251c575f64dc90c4db20316a0d89eaf46283d
SebastianDVL <sebas.lpcay@gmail.com>
2022-07-10 19:57:43 -0500

    Se cambia diseño de info de videojuego

--- js/infoJuego.js
+++ js/infoJuego.js
@@ -1,43 +1,42 @@
 import { consumirAPI } from "./consumirAPI.js";
 
+console.log(new Date(Date.now()).toLocaleDateString())
+let info = await consumirAPI(`https://belkigames.herokuapp.com/api/v1/game/${window.location.search.substring(4)}`,{
+    method: "GET"
+})
 
+console.log(info)
 
-    let info = await consumirAPI(`https://belkigames.herokuapp.com/api/v1/game/${window.location.search.substring(4)}`,{
-        method: "GET"
-    })
+let inf =  document.querySelector('main')
+//inf.classList.add(`bg-[url('${info.data.imagenPrincipal}')]`)
 
-    console.log(info)
+let imagen  = info.data.imagenCartelera
+//document.querySelector('#poster').classList.add(`bg-[url('${info.data.imagenCartelera}')]`)
 
-    let inf =  document.querySelector('#info')
-    //inf.classList.add(`bg-[url('${info.data.imagenPrincipal}')]`)
+const colorThief = new ColorThief()
+let img = new Image() 
+img.addEventListener('load', function() {
+    let color = colorThief.getColor(img);
+    document.querySelector('body').style.background = `rgb(${color[0]},${color[1]},${color[2]})`
+    document.querySelector('#poster div').classList.add(`group-hover:bg-[rgb(${color[0]},${color[1]},${color[2]},0.4)]`)
+})
+    
+img.src  =`${imagen}`
+img.crossOrigin = 'Anonymous';
 
-    let imagen  = info.data.imagenCartelera
-    document.querySelector('#poster').classList.add(`bg-[url('${info.data.imagenCartelera}')]`)
+document.querySelector("#banner").classList.add(`bg-[url('${info.data.imagenPrincipal}')]`)
+inf.querySelector('h1').textContent = info.data.nombre
+inf.querySelector('h2').textContent = info.data.categoria
+inf.querySelector('p').textContent = info.data.descripcipn
+inf.querySelector('h3').textContent = info.data.fechaLanzamiento
+inf.querySelector('#genres').textContent = "$" + Intl.NumberFormat("ES-co").format(info.data.precio)
 
-    const colorThief = new ColorThief()
-    let img = new Image() 
-    img.addEventListener('load', function() {
-        let color = colorThief.getColor(img);
-        document.querySelector('#info div').style.background = `rgb(${color[0]},${color[1]},${color[2]},0.9)`
-        document.querySelector('#poster div').classList.add(`group-hover:bg-[rgb(${color[0]},${color[1]},${color[2]},0.4)]`)
-    })
-      
-    img.src  =`${imagen}`
-    img.crossOrigin = 'Anonymous';
-   
-    document.querySelector("#banner").classList.add(`bg-[url('${info.data.imagenPrincipal}')]`)
-    inf.querySelector('h1').textContent = info.data.nombre
-    inf.querySelector('h2').textContent = info.data.categoria
-    inf.querySelector('p').textContent = info.data.descripcipn
-    inf.querySelector('h3').textContent = info.data.fechaLanzamiento
-    inf.querySelector('#genres').textContent = "$" + Intl.NumberFormat("ES-co").format(info.data.precio)
+const imgsContainer = document.querySelector('#imgs')
+info.data.imagenes.forEach(img =>{
+    let imgContainer = document.querySelector('[data-img]').content.cloneNode(true).children[0]
 
-    const imgsContainer = document.querySelector('#imgs')
-    info.data.imagenes.forEach(img =>{
-        let imgContainer = document.querySelector('[data-img]').content.cloneNode(true).children[0]
-   
-        imgContainer.querySelector('.h-80').classList.add(`bg-[url('${img}')]`)
-        
-        imgsContainer.appendChild(imgContainer)
-    })
+    imgContainer.querySelector('.h-80').classList.add(`bg-[url('${img}')]`)
+    
+    imgsContainer.appendChild(imgContainer)
+})
 
